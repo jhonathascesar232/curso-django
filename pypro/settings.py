e4f07029e7962bf72da7361b67f9afbbb10afb14
Jhonathas César <jhonathascesar232@gmail.com>
2021-03-12 20:15:39 -0300

    Correção flake8

--- pypro/settings.py
+++ pypro/settings.py
@@ -136,18 +136,20 @@ if AWS_ACCESS_KEY_ID:
     AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
     # CONTROLE DO TEMPO DE CACHE DO S3'
-    AWS_S3_OBJECT_PARAMETERS = {'CacheControl':'max-age=86400',}
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400', }
+
     AWS_PRELOAD_METADATA = True
     AWS_AUTO_CREATE_BUCKET = False
     AWS_QUERYSTRING_AUTH = True
     AWS_S3_CUSTOM_DOMAIN = None
+    
     # AWS 
     COLLECT_FAST_ENABLED = True
 
     AWS_DEFAULT_ACL = 'private'  # para os arquivos do s3 não ficarem públicos
 
     # Configurações dos arquivos estáticos
-    #---------------------------------------------------------
+    # ---------------------------------------------------------
     STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
     STATIC_S3_PATH = "static"
     STATIC_ROOT = f'//{STATIC_S3_PATH}/'
@@ -155,7 +157,7 @@ if AWS_ACCESS_KEY_ID:
     ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
     # Configurações de upload de media
-    #---------------------------------------------------------
+    # ---------------------------------------------------------
     DEFAULT_FILE_STORAGE = 's3_folder_storage.s3.DefaultStorage'
     DEFAULT_S3_PATH = "media"
     MEDIA_ROOT = f'//{DEFAULT_S3_PATH}/'
