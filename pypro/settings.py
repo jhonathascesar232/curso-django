b7799004795464a52495265b336ff604d6908c17
Jhonathas César <jhonathascesar232@gmail.com>
2021-03-12 20:21:43 -0300

    Flake8: correção

--- pypro/settings.py
+++ pypro/settings.py
@@ -142,8 +142,7 @@ if AWS_ACCESS_KEY_ID:
     AWS_AUTO_CREATE_BUCKET = False
     AWS_QUERYSTRING_AUTH = True
     AWS_S3_CUSTOM_DOMAIN = None
-    
-    # AWS 
+    # AWS
     COLLECT_FAST_ENABLED = True
 
     AWS_DEFAULT_ACL = 'private'  # para os arquivos do s3 não ficarem públicos
